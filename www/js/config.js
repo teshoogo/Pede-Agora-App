3e2881b9e39151fb90c9af0d5a8828a38e43f201
weslleyth15 <50183367+weslleyth15@users.noreply.github.com>
2019-05-01 21:56:56 -0300

    Atualização

---
+++ www/js/config.js
@@ -0,0 +1,6 @@
+var krms_config ={		
+	'ApiUrl':"http://pedeagora.hospteste.ga/mobileapp/api",				
+	'DialogDefaultTitle':"Notificação!",	
+	'APIHasKey':"df54asd654f56sd4fa8d7fsad5f4s6d4f8sdf",
+	'debug': false
+};
